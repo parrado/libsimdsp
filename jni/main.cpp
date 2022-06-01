5d979c88a947859b3644850ece8c6df4b6d33abf
parrado <parrado@uniquindio.edu.co>
2022-06-01 09:03:27 -0500

    dsp_loop() is no longer needed

--- jni/main.cpp
+++ jni/main.cpp
@@ -1,14 +1,12 @@
 #include "sharedmem.h"
 #include <unistd.h>
 void __attribute__((weak)) dsp_setup();
-void __attribute__((weak)) dsp_loop();
 
 int main(){
 main_client();
 dsp_setup();
 
 while(true){
-dsp_loop();
 usleep(10000);
 }
 
